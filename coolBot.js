24ae2e2a30a771a628d49c924f5bd5380c731749
Dimitrije11 <mitatrickovic7@gmail.com>
2015-03-08 17:12:08 +0100

    Update coolBot.js

--- coolBot.js
+++ coolBot.js
@@ -233,7 +233,7 @@
             etaRestriction: false,
             welcome: false,
             opLink: null,
-            rulesLink: ''https://sites.zoho.com/builder?siteId=49222106#pages/contactnull'',
+            rulesLink: ''https://www.sites.zoho.com/builder?siteId=49222106#pages/contactnull'',
             themeLink: null,
             fbLink: "https://www.facebook.com/groups/319438554929150/",
             youtubeLink: "https://www.youtube.com/channel/UCHpFcD7Bax4coW1KiyihTIQ",
