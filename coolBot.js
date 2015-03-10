2178421d8782b87ff75e75e026168ebee1ca2c90
Dimitrije11 <mitatrickovic7@gmail.com>
2015-03-10 20:59:09 +0100

    Update coolBot.js

--- coolBot.js
+++ coolBot.js
@@ -3023,7 +3023,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                    	API.sendChat("/me @" + chat.un + "Kralj čovek! Uvek je tu da pomogne.");
+                    	    API.sendChat("/me @" + chat.un + "Kralj čovek! Uvek je tu da pomogne.");
                     }
                 }
             };
