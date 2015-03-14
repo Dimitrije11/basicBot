a98a06c8b9e5dfa98f6bf8197c3424f6b0138678
Dimitrije11 <mitatrickovic7@gmail.com>
2015-03-14 14:20:24 +0100

    Update coolBot.js

--- coolBot.js
+++ coolBot.js
@@ -3040,6 +3040,19 @@
                 }
             },
             
+            opasnabrooCommand: {
+                command: 'opasnabroo',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                    	API.sendChat("/me @" + chat.un + " Naša opasnica, spremna je da banuje svakog!");
+                    }
+                }
+            },
+            
             youtubeCommand: {
                 command: 'youtube',
                 rank: 'user',
