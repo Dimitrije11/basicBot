431ab8949caac87f3c61a51b9fc10201e10060bc
Dimitrije11 <mitatrickovic7@gmail.com>
2015-04-01 12:52:29 +0200

    Update coolBot.js

--- coolBot.js
+++ coolBot.js
@@ -1926,7 +1926,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        var link = "http://i.imgur.com/SBAso1N.jpg";
+                        var link = "http://i.imgur.com/Z8aRW3v.png";
                         API.sendChat(subChat(basicBot.chat.starterhelp, {link: link}));
                     }
                 }
