ace00dca82a20a503d1e14ae6aa130402ed7bc63
Dimitrije11 <mitatrickovic7@gmail.com>
2015-04-02 10:51:07 +0200

    Update coolBot.js

--- coolBot.js
+++ coolBot.js
@@ -1893,7 +1893,7 @@
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
                         if (typeof basicBot.settings.fbLink === "string")
-                            API.sendChat("/me @" + chat.un + " Učlani se u našu FB grupu ! https://www.facebook.com/pages/Dexrock/406254506140493?fref=ts");
+                            API.sendChat("/me @" + chat.un + " Lajkuj FB stranicuu <3 ! https://www.facebook.com/pages/Dexrock/406254506140493?fref=ts");
                     }
                 }
             },
